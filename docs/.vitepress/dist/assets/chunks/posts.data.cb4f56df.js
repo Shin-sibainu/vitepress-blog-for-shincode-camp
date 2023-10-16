63d607f4bfa7fd32f243250daea168a6934484a8
Shin-sibainu <shincode0712@gmail.com>
2023-10-16 13:22:43 +0900

    mod

---
+++ docs/.vitepress/dist/assets/chunks/posts.data.cb4f56df.js
@@ -0,0 +1,1 @@
+const t=JSON.parse('[{"frontmatter":{"theme":"page","title":"VitePressを使ってブログを構築してみた","next":false,"prev":false},"url":"/posts/blogposts/","relativePath":"/posts/blogposts/.md"},{"frontmatter":{"theme":"page","title":"Test","next":false,"prev":false},"url":"/posts/blogposts/test.html","relativePath":"/posts/blogposts/test.html.md"}]');export{t as d};
